6277127692bb0c4f1787b973e362a452fd694c72
Saraswathi-13 <2300032530@kluniversity.in>
2025-09-23 16:22:37 +0530

    updated springboot

--- SpringBootBackend/src/main/java/com/klef/practice/SpringBootBackendApplication.java
+++ SpringBootBackend/src/main/java/com/klef/practice/SpringBootBackendApplication.java
@@ -2,6 +2,7 @@ package com.klef.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootBackendApplication extends SpringBootServletInitializer{
