0db3bdd5bc6ec0b7cce40fd9ae9e0882c507cb7d
Saraswathi-13 <2300032530@kluniversity.in>
2025-09-23 16:19:03 +0530

    updated pom.xml

--- SpringBootBackend/src/main/java/com/klef/practice/SpringBootBackendApplication.java
+++ SpringBootBackend/src/main/java/com/klef/practice/SpringBootBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBackendApplication {
+public class SpringBootBackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
